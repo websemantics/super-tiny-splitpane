a62c70f7d792c612c4ee990f2942ed365e262057
Adnan <adnan@Adnans-MacBook-Pro.local>
2017-05-03 18:06:38 -0400

    First commit

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
-# super-tiny-splitpane
-Bare minimum implementation of a Splitpane UI with supports for horizontal and vertical layouts
+```
+╭─╮ ┬ ┬ ╭─╮ ╭─╮ ┬─╮   ╭┬╮ ┬ ╭╮╭ ╭─╮   ╭─╮ ╭─╮ ┬   ┬ ╭┬╮ ╭─╮ ╭─╮ ╭╮╭ ╭─╮  
+╰─╮ │ │ ├─╯ ├┤  ├┬╯    │  │ │││ ├┤    ╰─╮ ├─╯ │   │  │  ├─╯ ├─┤ │││ ├┤   
+╰─╯ ╰─╯ ┴   ╰─╯ ┴╰─    ┴  ┴ ╯╰╯ ╰─╯   ╰─╯ ┴   ┴─╯ ┴  ┴  ┴   ┴ ┴ ╯╰╯ ╰─╯  
+Under 2kb and without dependencies 
+```
+> Bare minimum implementation of a Splitpane UI with supports for horizontal and vertical layouts.
+
+[![Live demo](https://img.shields.io/badge/Live%20demo-%E2%86%92-9D6EB3.svg?style=flat-square)](https://websemantics.github.io/super-tiny-splitpane)
+
+## Examples
+
+- [Basic Horizontal](https://websemantics.github.io/super-tiny-splitpane/examples/basic-horizontal.html)
+- [Basic Vertical](https://websemantics.github.io/super-tiny-splitpane/examples/basic-vertical.html)
+
+## Support
+
+Need help or have a question? post a questions at [StackOverflow](https://stackoverflow.com/questions/tagged/super-tiny-splitpane+websemantics)
+
+*Please don't use the issue trackers for support/questions.*
+
+## Contribution
+
+More than happy to accept external contributions to the project in the form of feedback, bug reports and even better - pull requests :)
+
+## License
+
+[MIT license](http://opensource.org/licenses/mit-license.php)
+Copyright (c) Web Semantics, Inc.
---
+++ splitpane.js
@@ -0,0 +1,172 @@
+/**
+ * 
+ * ╭─╮ ┬ ┬ ╭─╮ ╭─╮ ┬─╮   ╭┬╮ ┬ ╭╮╭ ╭─╮   ╭─╮ ╭─╮ ┬   ┬ ╭┬╮ ╭─╮ ╭─╮ ╭╮╭ ╭─╮  
+ * ╰─╮ │ │ ├─╯ ├┤  ├┬╯    │  │ │││ ├┤    ╰─╮ ├─╯ │   │  │  ├─╯ ├─┤ │││ ├┤   
+ * ╰─╯ ╰─╯ ┴   ╰─╯ ┴╰─    ┴  ┴ ╯╰╯ ╰─╯   ╰─╯ ┴   ┴─╯ ┴  ┴  ┴   ┴ ┴ ╯╰╯ ╰─╯  
+ * Under 2kb and without dependencies 
+ * 
+ * Bare minimum implementation of a Splitpane UI with supports for 
+ * horizontal and vertical layouts
+ * 
+ * #### Note
+ * The following is the minimum markup and style rules to use Splitpane
+ *
+ *  <div class="splitpane">                 <!-- for vertical splitpane add  `vertical` -->
+ *    <div style="width: 50%"></div>        <!-- class and set `height` of first child -->
+ *    <div class="handle"></div>
+ *   <div></div>
+ *  </div> 
+ * 
+ *  <style>
+ *    html, body { height: 100% }
+ *    .splitpane {width: 100%; height: 100% }
+ *    .handle {background-color: lightgrey; }
+ *    .handle:hover {background-color: grey }
+ *  </style>
+ * 
+ * @copyright (c) 2016-2017, Web Semantics, Inc.
+ * @author Adnan M.Sagar, PhD. <adnan@websemantics.ca>
+ * @license Distributed under the terms of the MIT License.
+ */
+
+(function (exports) {
+    'use strict'
+
+    var defaults = {  /* Library defaults, change using the `defaults` static method */
+        disable: false,
+        handle: 5,
+        minHeight: 100,
+        minWidth:100,
+        templates : [
+        [ /* vertical layout: top, handle and bottom */
+        'left:0; right:0; top:0; height: {ratio}%; padding-bottom: {half}px',
+        'left:0; right:0; top:{ratio}%; width: 100%; margin-top: -{half}px; height: {handle}px; cursor: {cursor}; z-index: 1',
+        'left:0; right:0; bottom:0; height: {supplement}%; padding-top: {half}px'],
+        [ /* horizontal layout: left, handle and right */
+        'top:0; bottom:0; left:0; width: {ratio}%; padding-right: {half}px',
+        'top:0; bottom:0; left:{ratio}%; margin-left: -{half}px; width: {handle}px; cursor: {cursor}; z-index: 1',
+        'top:0; bottom:0; right:0; width: {supplement}%; padding-left: {half}px']],
+        cursor: ['row-resize', 'col-resize']}
+
+    /**
+     * Create a Splitpane instance.
+     *
+     * @param {Node} el - Splitpane DOM node
+     * @param {Number} minHeight - Smallest pane height (top/bottom)
+     * @param {Number} minWidth - Smallest pane height (left/right)
+     * 
+     * #### Note
+     * Process splitpanes, calculate width or height of the handle (depending on orientation) and apply the 
+     * appropriate styles to children
+     * 
+     * #### Example
+     * ```js
+     * var split = new Splitpane( '#app' )
+     * ```
+     * @property {Number} orien - Splitpane orientation: 0 = vertical, 1 = horizontal (*defult)
+     * @property {Number} min - Splitpane pane's minimum width (horizontal) or height (vertical)
+     * @property {ClientRect} container - Splitpane bounding client rectangle
+     * @property {NodeList} children - Splitpane children, [0]=top/left pane, [1]=handle, [2]=bottom/right pane
+     * @property {String} cursor - Handle mouse cursor value
+     * @property {Number} handle - Handle thickness (`width` or `height` depending on orientation)
+     * @constructor
+     */
+    var self = exports.Splitpane = function (el, options) {
+
+        var orien      = el.classList.contains('vertical') ? 0 : 1
+        var min        = defaults[['minHeight', 'minWidth'][orien]]
+        var container  = el.getBoundingClientRect()
+        var children   = el.querySelectorAll(':scope > div')
+        var cursor     = window.getComputedStyle(children[1]).getPropertyValue('cursor')
+        var handle     = children[1].getBoundingClientRect()[['height', 'width'][orien]]
+        
+        /* set defaults for handle thickness and mouse cursor if not set */
+        handle = handle < 1 || handle === container[['height', 'width'][orien]] ? defaults.handle : handle
+        cursor = cursor === 'auto' ? defaults.cursor[orien] : cursor,
+
+        /* Handle */ children[1].addEventListener('mousedown', function (event) {
+            var max = container[['height', 'width'][orien]] - min
+
+            var listeners = [ 
+                function mousemove(event) {  
+                    var xy = event[['pageY', 'pageX'][orien]] - container[['top', 'left'][orien]]
+
+                    event.preventDefault()
+                    _compile(( xy > min ? xy < max ? xy : max : min ) / container[['height', 'width'][orien]])
+                },
+                function mouseup( event ) { 
+                    listeners.forEach(function (l) { document.removeEventListener(l.name, l)
+                    window.dispatchEvent(new Event('resize')) /* inform everybody */ }) 
+            }]
+            listeners.forEach(function (l) { document.addEventListener(l.name, l) }) 
+        })
+
+        /**
+         * @function _compile - Updates Splitpane children dimension
+         * 
+         * @param {Number} ratio - The portion (a percentage) that the first child (top/left)
+         *                         occupies within the overall width/height of the parent pane
+         * @private
+         */
+        function _compile(ratio) {
+            children.forEach(function (child, i) {
+                child.style = 'position: absolute; ' + _mustach( defaults.templates[orien][i], {
+                    half:  handle / 2, 
+                    handle: handle,
+                    cursor: cursor,
+                    ratio: ratio  * 100,
+                    supplement: (1 - ratio) * 100
+                }) 
+            })
+        }
+
+        /**
+         * @function _mustach -  Super tiny mustach-like template parser with nested paths support (i.e. x.y.z)
+         * 
+         * @param {String} template - Template string
+         * @param {Object} data - Context data
+         * @param {strig} path - Nestet path prefix
+         * @returns {String}
+         * @private
+         */
+
+        function _mustach(template, data, path) {
+            path = path || ''
+            Object.keys(data || {}).forEach(function (key) {
+                template = typeof data[key] === 'object' ?
+                    _mustach(template, data[key], path + key + '.') :
+                    template.replace(new RegExp("{ *?" + path + key + " *?}", 'g'), data[key])
+            })
+            return template
+        }
+
+        /**
+         * @function resize -  Public function to adjust splitpane size
+         * @public
+         */
+        this.resize = function(){
+            container = el.getBoundingClientRect()
+        }
+
+        _compile( children[0].getBoundingClientRect()[['height', 'width'][orien]] / 
+                  container[['height', 'width'][orien]])
+    }
+
+    /**
+     * @function defaults - Override class defaults
+     * 
+     * @param {Object} options - Name value pairs
+     * @static
+     */
+    self.defaults = function (options) { 
+        Object.keys( options || {} ).forEach( function( key ){ defaults[key] = options[key] })
+    }
+
+    window.addEventListener("load", function(event) { /* activate all splitpanes, register resize event */
+        var panes = [].slice.call(document.querySelectorAll('.splitpane') || []).map(
+                            function (splitpane) { return new Splitpane(splitpane) })
+        
+        panes.length > 0 && !defaults.disable && window.addEventListener('resize', 
+                            function(event){ panes.forEach(function(pane){ pane.resize() })}, true)
+    })
+})(this);
